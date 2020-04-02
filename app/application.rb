08fecd5a62610acbdf2538221aa1e8d9c5f7177c
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-02 15:39:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,10 @@
-require 'pry'
+
 class Application
   def call(env)
     resp = Rack::Response.new
 
     if Time.now.to_i <= 12
-      binding.pry
+
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"

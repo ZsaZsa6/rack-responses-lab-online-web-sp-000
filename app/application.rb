f17ed20fd9387fb929f64dbf33f326575679485f
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-02 15:06:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.now.to_i ).zero? ?  ["<em>Good Morning</em>"] : ["<strong>Good Afternoon</strong>"]
+    if Time.now.to_i ).zero? ?
+      "<em>Good Morning</em>"] : ["<strong>Good Afternoon</strong>"]
   end
 end

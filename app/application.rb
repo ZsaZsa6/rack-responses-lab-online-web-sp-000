4c275a00e0fb907ee7bddd309e18eb7f471c616f
Ronda D Cowen <cowenhome5@gmail.com>
2020-04-02 15:09:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.now.to_i 
+    if Time.now.to_i <= 1159
       resp write "Good Morning"
     else
       resp write "Good Afternoon"
